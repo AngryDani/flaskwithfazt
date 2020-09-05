eb199d00dcaca038ace306794386db1a9ae4e210
AngryDan <a.vidal.analyst@gmail.com>
2020-09-05 16:55:10 -0500

    corriendo la app 1° paso

--- index.py
+++ index.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 app = Flask(__name__) # Inicializar Flash + confirmar que es el archivo principal, se guarda en la variable app (crear rutas del servidor + url)
 
+# Crear una ruta con un decorador
+@app.route('/') #creo una ruta para la página principal
+def home():
+    return "Holiwis :D"
+
+
+if __name__=="__main__":
+    app.run()
+
 
 
