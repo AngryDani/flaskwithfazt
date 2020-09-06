0c6290bcd4f60e8f8394cd433a99b3d6165f5907
AngryDan <a.vidal.analyst@gmail.com>
2020-09-05 19:16:53 -0500

    Creo la carpeta templates para almacenar los html e importo render_template para inicializar dichos documentos

--- index.py
+++ index.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # Inicializar Flash + confirmar que es el archivo principal, se guarda en la variable app (crear rutas del servidor + url)
 
 # Crear una ruta con un decorador
 @app.route('/') #creo una ruta para la página principal
 def home():
-    return "HOME PAGE\nHoliwis :D"
+    return render_template("home.html")
 
 @app.route('/about') # Creando el ABOUT localhost:5000/about
 def about():
     return "ABOUT: @ANGRYDANY"
 
+# Creamos la carpeta templates para almacenar los documenetos html e importamos render_template
 
 
 
